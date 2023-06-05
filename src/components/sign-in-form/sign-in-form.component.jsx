c9388b87c1fc589258bac6c307afad0121793f6d
EfratGrinberg <efratg@one1.co.il>
2023-06-05 09:49:17 +0300

    finish authentication

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+  createUserWithEmailAndPassword,
+  signInWithGooglePopup,
+  signInAuthWithEmailAndPassword
+} from "../../utils/firebase/firebase.utils";
+import FormInput from "../form-input/form-input.component";
+import "./sign-in.styles.scss";
+import Button from "../button/button.component";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: ""
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormField = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const SignInWIthGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  const HandleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await signInAuthWithEmailAndPassword(email, password);
+      console.log(response);
+      resetFormField();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("Incorrect password for email");
+          break;
+        case "auth/user-not-found":
+          alert("Email isnt found");
+          break;
+        default:
+          console.log(error.code);
+      }
+
+      if (error.code == "auth/wrong-password") {
+        alert("Incorrect password for email");
+      }
+      if (error.code == "auth/user-not-found") {
+        alert("Email isnt found");
+      }
+    }
+  };
+
+  const HandleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>Already have an account</h2>
+      <span>sign in with your email & password</span>
+      <form onSubmit={HandleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={HandleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={HandleChange}
+          name="password"
+          value={password}
+        />
+
+        <div className="button-container">
+          <Button type="submit">Sign In</Button>
+          <Button type="button" buttonType="google" onClick={SignInWIthGoogle}>
+            Google sign in
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default SignInForm;
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -7,29 +7,20 @@ import {
   signInWithGoogleRedirect
 } from "../../utils/firebase/firebase.utils";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-
-const SignIn = () => {
-  // useEffect(async () => {
-  //   const response = await getRedirectResult(auth);
-  //   if (response) {
-  //     const userDocRef = await createUserDocumentFromAuth(
-  //       response.operationTypeuser
-  //     );
-  //   }
-  // }, []);
-
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import './authentication.styles.scss'
+const Authentication = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
-    <div>
-      <h1>Sign IN Page</h1>
-      <button onClick={logGoogleUser}>Sign in with google popup</button>
+    <div className="authentication-container">
+      <SignInForm/>      
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
