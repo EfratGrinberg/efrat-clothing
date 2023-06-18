2e0c18143849d4f771094e714045d43bcab559d9
EfratGrinberg <efratg@one1.co.il>
2023-06-18 11:49:52 +0300

    adding payment

--- src/index.js
+++ src/index.js
@@ -4,19 +4,21 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
-import { persistor, store } from './store/store';
-
+import { persistor, store } from "./store/store";
+import { Elements } from "@stripe/react-stripe-js";
+import {stripePromise} from './utils/stripe/stripe.utils';
 import "./index.scss";
 
 const rootElement = document.getElementById("root");
 
-// Use createRoot instead of render
 createRoot(rootElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
